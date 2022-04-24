abe1344f8187656f22ed9f603f723654f8148162
yadul <95083929+man-yadul@users.noreply.github.com>
2022-04-24 11:13:37 +0530

    Update SumOfDigits.java

--- Week 1: Basic Programs I/SumOfDigits.java
+++ Week 1: Basic Programs I/SumOfDigits.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
-public class SumOfDigits 
-{
-    public static void main(String args[])
-    {
+
+public class SumOfDigits {
+    public static void main(String args[]) {
         int m, n, sum = 0;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the number: ");
         m = s.nextInt();
-        while(m > 0)
-        {
+        while (m > 0) {
             n = m % 10;
             sum = sum + n;
             m = m / 10;
