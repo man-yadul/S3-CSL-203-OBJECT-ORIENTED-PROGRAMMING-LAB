1127041487f32a9ed8fe0718fa291c4b1527ce26
yadul <95083929+man-yadul@users.noreply.github.com>
2022-04-24 11:11:50 +0530

    Update Frequency.java

--- Week 2: Basic Programs II - Strings/Frequency.java
+++ Week 2: Basic Programs II - Strings/Frequency.java
@@ -5,17 +5,17 @@ public class Frequency {
         Scanner sc = new Scanner(System.in);
         int k = 0;
         char c;
-        
+
         System.out.print("Enter string: ");
         String str = sc.nextLine();
         System.out.print("Enter character to search: ");
         c = sc.next().charAt(0);
-        
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == c)
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c)
                 k++;
         }
-        
+
         System.out.println("The character " + c + " has occured " + k + " times.");
     }
 }
